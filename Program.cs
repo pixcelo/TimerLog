d7e0121ebbefe8f33ab8f863257ab5e9d25a3a20
pixcelo <bodomu@gmail.com>
2022-11-10 04:50:35 +0900

    add: files

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace jimu
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static async Task GetProductAsync(string path)
+        {
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                File.WriteAllText("candle.json", json);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            try
+            {
+                string url = "https://api.cryptowat.ch/markets/bybit/BTCUSDT/ohlc?after=1667239230";
+                await GetProductAsync(url);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(@$"{e.Message},
+                                {Environment.NewLine}
+                                {e.StackTrace}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
