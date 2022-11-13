4f5156e5aad58848379c6d5aea1b10544da57022
pixcelo <bodomu@gmail.com>
2022-11-13 10:57:42 +0900

    update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
 
         static long GenerateUnixTime()
         {
-            DateTimeOffset dto = new DateTimeOffset(2022, 11, 1, 15, 30, 0, TimeSpan.Zero);
+            DateTimeOffset dto = new DateTimeOffset(2022, 10, 1, 15, 30, 0, 0, TimeSpan.Zero);
             return dto.ToUnixTimeSeconds();
         }
 
@@ -18,13 +18,13 @@
             return $"https://api.cryptowat.ch/markets/{exchange}/{pair}/ohlc?after={unixTime}";
         }
 
-        static async Task GetProductAsync(string path)
+        static async Task GetProductAsync(string path, long requestTime)
         {
             HttpResponseMessage response = await client.GetAsync(path);
             if (response.IsSuccessStatusCode)
             {
                 var json = await response.Content.ReadAsStringAsync();
-                File.WriteAllText("candle.json", json);
+                File.WriteAllText($"{requestTime}.json", json);
             }
         }
 
@@ -37,9 +37,16 @@
         {
             try
             {
-                long unixTime = GenerateUnixTime();
-                string url = GeneratePath(unixTime);
-                await GetProductAsync(url);
+                for (int i = 0; i < 3; i++)
+                {
+                    const int ADD_SECOUNDS = 30000;
+                    long unixTime = GenerateUnixTime();
+                    long requestTime = unixTime + (i * ADD_SECOUNDS);
+                    string url = GeneratePath(requestTime);
+                    await GetProductAsync(url, requestTime);
+                }
+
+                return;
             }
             catch (Exception e)
             {
