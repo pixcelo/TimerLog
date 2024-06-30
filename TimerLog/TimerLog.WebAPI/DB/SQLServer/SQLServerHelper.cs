063451020101377fdfa5306c499e43ece88dfe5e
pixcelo <bodomu@gmail.com>
2024-07-01 05:38:40 +0900

    add: DBヘルパ

---
+++ TimerLog/TimerLog.WebAPI/DB/SQLServer/SQLServerHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.SqlClient;
+
+namespace TimerLog.WebAPI.DB.SQLServer
+{
+    public class SQLServerHelper
+    {
+        /// <summary>
+        /// 接続文字列を取得する
+        /// </summary>
+        /// <param name="server"></param>
+        /// <param name="database"></param>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string GetConnectionString(
+            string server,
+            string database,
+            string user,
+            string password)
+        {
+            var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = server;
+            builder.InitialCatalog = database;
+            builder.UserID = user;
+            builder.Password = password;
+
+            return builder.ConnectionString;
+        }
+
+        /// <summary>
+        /// Windows認証を使用して接続文字列を取得する
+        /// </summary>
+        /// <param name="server">サーバー名</param>
+        /// <param name="database">データベース名</param>
+        /// <returns>接続文字列</returns>
+        public static string GetConnectionStringWithWindowsAuth(
+            string server,
+            string database)
+        {
+            var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = server;
+            builder.InitialCatalog = database;
+            builder.IntegratedSecurity = true;
+
+            return builder.ConnectionString;
+        }
+    }
+}
