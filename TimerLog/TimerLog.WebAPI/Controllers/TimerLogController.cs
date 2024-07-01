38f2a445868801fb3f115a1c55d0c674081c3a5b
pixcelo <bodomu@gmail.com>
2024-07-02 03:32:32 +0900

    add: タイマーログAPIコントローラ

---
+++ TimerLog/TimerLog.WebAPI/Controllers/TimerLogController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TimerLog.WebAPI.Models;
+using TimerLog.WebAPI.Services;
+
+namespace TimerLog.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimerLogController : ControllerBase
+    {
+        private readonly ITimerLogService timerLogService;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="timerLogService"></param>
+        public TimerLogController(ITimerLogService timerLogService)
+        {
+            this.timerLogService = timerLogService;
+        }
+
+        [HttpGet]
+        public List<V_TimerLog> Get()
+        {
+            return this.timerLogService.Find();
+        }
+
+        [HttpPost]
+        public int Post([FromBody] T_TimerLog timerLog)
+        {
+            return this.timerLogService.Insert(timerLog);
+        }
+
+        [HttpPut]
+        public int Put([FromBody] T_TimerLog timerLog)
+        {
+            return this.timerLogService.Update(timerLog);
+        }
+
+        [HttpDelete("{id}")]
+        public int Delete(long id)
+        {            
+            return this.timerLogService.Delete(id);
+        }
+
+    }
+}
