9229f7204e329bb3249f666fbe7ef435fee56cb4
pixcelo <bodomu@gmail.com>
2024-07-06 18:04:36 +0900

    mod: コントローラのテストを追加

--- TimerLog/TimerLog.Tests/Controllers/TimerLogControllerTests.cs
+++ TimerLog/TimerLog.Tests/Controllers/TimerLogControllerTests.cs
@@ -33,5 +33,59 @@ namespace TimerLog.Tests.Controllers
             Assert.Same(expected, actual);
         }
 
+        /// <summary>
+        /// 正常系
+        /// </summary>
+        [Fact]
+        public void OkPost()
+        {
+            // Arrange
+            var expected = 1;
+            var timerLog = new T_TimerLog();
+            this.mockTimerLogService.Setup(x => x.Insert(timerLog)).Returns(expected);
+
+            // Act
+            var actual = this.controller.Post(timerLog);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// 正常系
+        /// </summary>
+        [Fact]
+        public void OkPut()
+        {
+            // Arrange
+            var expected = 1;
+            var timerLog = new T_TimerLog();
+            this.mockTimerLogService.Setup(x => x.Update(timerLog)).Returns(expected);
+
+            // Act
+            var actual = this.controller.Put(timerLog);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// 正常系
+        /// </summary>
+        [Fact]
+        public void OkDelete()
+        {
+            // Arrange
+            var expected = 1;
+            var id = 1;
+            this.mockTimerLogService.Setup(x => x.Delete(id)).Returns(expected);
+
+            // Act
+            var actual = this.controller.Delete(id);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        
     }
 }
