f67e7704bd1e8e39960ff8c31a079440dd6b155e
tetsu.k <bodomu@gmail.com>
2024-07-15 04:20:10 +0900

    mod: ストップウォッチログのテーブル・ビュー

--- Schema/Create_StopwatchLogs.sql
+++ Schema/Create_StopwatchLogs.sql
@@ -1,5 +1,24 @@
 CREATE TABLE T_StopwatchLogs (
     LogId INT IDENTITY(1,1) PRIMARY KEY,
     ElapsedTime BIGINT NOT NULL, -- 経過時間をミリ秒単位で記録
-    LogDate DATETIME NOT NULL -- 記録日時
+    LogDate DATETIME NOT NULL, -- 記録日時
+    UserId BIGINT NOT NULL, -- ユーザーID
+    TypeId BIGINT NULL, -- タイプID
 );
+
+-- ストップウォッチログビュー
+CREATE VIEW V_StopwatchLogs AS
+SELECT
+    T.LogId,
+    T.ElapsedTime,
+    T.logDate,
+    T.userId,
+    T.typeId,
+    U.Name AS UserName,
+    TT.Name AS TimerTypeName
+FROM
+    T_StopwatchLogs T
+    LEFT JOIN M_Users U ON T.UserId = U.Id
+    LEFT JOIN M_TimerType TT ON T.TypeId = TT.Id;
+
+GO
