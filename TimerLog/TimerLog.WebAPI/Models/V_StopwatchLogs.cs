7d8186d5eb6e69d715dcfa47f101651e7fe0f85a
tetsu.k <bodomu@gmail.com>
2024-07-15 06:53:01 +0900

    add: 経過時間を分:秒.ミリ秒で表示するプロパティ

--- TimerLog/TimerLog.WebAPI/Models/V_StopwatchLogs.cs
+++ TimerLog/TimerLog.WebAPI/Models/V_StopwatchLogs.cs
@@ -6,6 +6,11 @@
     public class V_StopwatchLogs : T_StopwatchLogs
     {
         /// <summary>
+        /// 経過時間（フォーマット済み）
+        /// </summary>
+        public string? ElapsedTimeFormatted { get; set; }
+
+        /// <summary>
         /// ユーザー名
         /// </summary>
         public string? UserName { get; set; }
