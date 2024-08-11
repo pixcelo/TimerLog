7986c9ffc26c48ce3928a3d7668fb6646f0788b7
tetsu.k <bodomu@gmail.com>
2024-08-11 16:02:01 +0900

    add: タイマーログコンテキスト

---
+++ TimerLogBlazorApp/DB/TimerLogContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimerLogBlazorApp.Models;
+
+namespace TimerLogBlazorApp.DB
+{
+    public sealed class TimerLogContext : DbContext
+    {
+        public DbSet<T_TimerLogs> T_TimerLogs { get; set; }
+        public DbSet<V_TimerLogs> V_TimerLogs { get; set; }
+        public DbSet<M_Users> M_Users { get; set; }
+        public DbSet<M_TimerTypes> M_TimerTypes { get; set; }
+
+        public TimerLogContext(DbContextOptions<TimerLogContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<T_TimerLogs>()
+                .UseTptMappingStrategy();
+        }
+    }
+}
