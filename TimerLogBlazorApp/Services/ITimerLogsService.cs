67091eaf78395548e68c18d90d6d478047397e47
tetsu.k <bodomu@gmail.com>
2024-08-11 16:02:56 +0900

    add: タイマーログサービス

---
+++ TimerLogBlazorApp/Services/ITimerLogsService.cs
@@ -0,0 +1,25 @@
+﻿using TimerLogBlazorApp.Attributes;
+using TimerLogBlazorApp.Models;
+
+namespace TimerLogBlazorApp.Services
+{
+    /// <summary>
+    /// タイマーログサービスのインターフェイス
+    /// </summary>
+    [Component]
+    public interface ITimerLogsService
+    {
+        /// <summary>
+        /// タイマーログを取得する
+        /// </summary>
+        /// <returns></returns>
+        List<V_TimerLogs> Find();
+
+        /// <summary>
+        /// タイマーログを登録する
+        /// </summary>
+        /// <param name="timerLog"></param>
+        /// <returns></returns>
+        int Insert(T_TimerLogs timerLog);
+    }
+}
---
+++ TimerLogBlazorApp/Services/TimerLogsService.cs
@@ -0,0 +1,33 @@
+﻿using TimerLogBlazorApp.Models;
+using TimerLogBlazorApp.Repositories;
+
+namespace TimerLogBlazorApp.Services
+{
+    /// <summary>
+    /// タイマーログサービス
+    /// </summary>
+
+    public sealed class TimerLogsService : ITimerLogsService
+    {
+        private readonly ITimerLogsRepository timerLogsRepository;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="timerLogsRepository"></param>
+        public TimerLogsService(ITimerLogsRepository timerLogsRepository)
+        {
+            this.timerLogsRepository = timerLogsRepository;
+        }
+
+        public List<V_TimerLogs> Find()
+        {
+            return this.timerLogsRepository.Find();
+        }
+
+        public int Insert(T_TimerLogs timerLog)
+        {
+            return this.timerLogsRepository.Insert(timerLog);
+        }
+    }
+}
